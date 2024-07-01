456f3242c55758874bfa50773384afba1605a0df
jskinner <jskinner18@cnm.edu>
2024-07-01 12:01:38 -0600

    cleaned up game controller, guess controller, created guess repository.

--- src/main/java/edu/cnm/deepdive/codebreaker/controller/GuessController.java
+++ src/main/java/edu/cnm/deepdive/codebreaker/controller/GuessController.java
@@ -1,10 +1,13 @@
 package edu.cnm.deepdive.codebreaker.controller;
 
+import edu.cnm.deepdive.codebreaker.model.entity.Game;
 import edu.cnm.deepdive.codebreaker.model.entity.Guess;
+import edu.cnm.deepdive.codebreaker.service.AbstractGameService;
 import edu.cnm.deepdive.codebreaker.service.AbstractUserService;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,31 +20,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/games/{gameKey}/guesses")
 public class GuessController {
 
+  private final AbstractGameService gameService;
   private final AbstractUserService userService;
 
-  // TODO: 7/1/24 : Declare and initialize (in constructor) any additional service dependencies
   @Autowired
-  public GuessController(AbstractUserService userService) {
+  public GuessController(AbstractGameService gameService, AbstractUserService userService) {
+    this.gameService = gameService;
     this.userService = userService;
   }
 
   @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
   public List<Guess> get(@PathVariable UUID gameKey) {
-    // TODO: 7/1/24 : Invoke game service method to retrieve and return list of guesses from specified game.
-    throw new UnsupportedOperationException();
+    return gameService.getGame(gameKey, userService.getCurrentUser())
+        .getGuesses();
   }
 
   @PostMapping(
       consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-  public Guess post(@PathVariable UUID gameKey, @RequestBody Guess guess) {
-    // TODO: 7/1/24 : Invoke game service method to validate and add specified guess to specified game.
-    throw new UnsupportedOperationException();
+  public Game post(@PathVariable UUID gameKey, @RequestBody Guess guess) {
+    // TODO: 7/1/24 : Return response with 201 Status and Location header.
+    return gameService.submitGuess(gameKey, guess, userService.getCurrentUser());
   }
 
   @GetMapping(path = "/{guessKey}", produces = MediaType.APPLICATION_JSON_VALUE)
-  public Guess get(@PathVariable UUID gameKey, @PathVariable UUID guessKey) {
-    // TODO: 7/1/24 : Invoke game service method to retrieve and return specified guess in specified game.
-    throw new UnsupportedOperationException();
+  public Game get(@PathVariable UUID gameKey, @PathVariable UUID guessKey) {
+    return gameService.getGuess(gameKey, guessKey, userService.getCurrentUser());
   }
 
 
